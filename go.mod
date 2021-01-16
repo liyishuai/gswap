c45065128b1d0f378d42312c6e63085f39a15136
Yishuai Li <yishuai@upenn.edu>
2021-01-16 10:39:25 -0500

    Create asset by ticker

--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
 module gswap
 
+// +heroku goVersion go1.15
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/lib/pq v1.3.0
+	gorm.io/driver/postgres v1.0.6
+	gorm.io/gorm v1.20.8
+)
--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+// Asset ...
+type Asset struct {
+	gorm.Model
+	Ticker string `gorm:"unique"`
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+	sqlDB, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening SQL database: %q", err)
+	}
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error opening Gorm database: %q", err)
+	}
 	r := gin.New()
+	r.Use(gin.Logger())
+	r.LoadHTMLGlob("templates/*.tmpl.html")
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "world",
-		})
+		var assets []Asset
+		db.Find(&assets)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"assets": assets})
+	})
+	r.POST("/newAsset", func(c *gin.Context) {
+		db.Create(&Asset{Ticker: c.PostForm("ticker")})
+		var assets []Asset
+		db.Find(&assets)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"assets": assets})
 	})
 	r.Run(":" + port)
 }
