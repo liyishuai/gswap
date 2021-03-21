334dd9b1d7f6dea2624c0791457a8bc39b04f8cd
Yishuai Li <yishuai@upenn.edu>
2021-03-20 22:14:13 -0400

    Take order

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,36 @@ import (
 	"gorm.io/gorm"
 )
 
-// Asset ...
+type User struct {
+	gorm.Model
+	Accounts []Account
+}
+
+type Account struct {
+	gorm.Model
+	UserID      uint   `gorm:"uniqueIndex:idx_acnt"`
+	AssetTicker string `gorm:"uniqueIndex:idx_acnt"`
+	Asset       Asset
+	Amount      uint `gorm:"default:0"`
+}
+
+type Order struct {
+	gorm.Model
+	BuyerID    string
+	Buyer      Account
+	BuyAmount  uint
+	SellerID   uint
+	Seller     Account
+	SellAmount uint
+}
+
 type Asset struct {
 	Ticker string `gorm:"primaryKey;size:3"`
+}
+
+type Taker struct {
+	UserID  uint `form:"user"`
+	OrderID uint `form:"order"`
 }
 
 func main() {
@@ -32,29 +60,93 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error opening Gorm database: %q", err)
 	}
+	db.AutoMigrate(&User{}, &Account{}, &Asset{}, &Order{})
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.tmpl.html")
 	r.GET("/", func(c *gin.Context) {
 		var assets []Asset
-		db.Find(&assets)
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"assets": assets})
+		if err := db.Find(&assets).Error; err != nil &&
+			!errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		} else {
+			c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"assets": assets})
+		}
 	})
 	r.POST("/newAsset", func(c *gin.Context) {
 		ticker := c.PostForm("ticker")
-		db.Create(&Asset{Ticker: ticker})
-		if db.Error != nil {
-			c.HTML(http.StatusBadRequest, "asset.tmpl.html", gin.H{"message": db.Error})
+		if err := db.Create(&Asset{Ticker: ticker}).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		} else {
-			c.HTML(http.StatusOK, "asset.tmpl.html", gin.H{"message": "已发行" + ticker})
+			c.Status(http.StatusCreated)
 		}
 	})
 	r.POST("/removeAsset", func(c *gin.Context) {
-		ticker := c.PostForm("ticker")
-		db.Delete(&Asset{}, "ticker = ?", ticker)
-		if db.Error != nil {
-			c.HTML(http.StatusBadRequest, "asset.tmpl.html", gin.H{"message": db.Error})
+		if err := db.Delete(&Asset{Ticker: c.PostForm("ticker")}); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		} else {
-			c.HTML(http.StatusOK, "asset.tmpl.html", gin.H{"message": "已撤销" + ticker})
+			c.Status(http.StatusNoContent)
+		}
+	})
+	r.POST("/takeOrder", func(c *gin.Context) {
+		insufficientFund := errors.New("insufficient fund")
+		var taker Taker
+		if err := c.ShouldBind(&taker); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		} else if err := db.Transaction(func(tx *gorm.DB) error {
+			var order Order
+			if err := db.Take(&order, taker.OrderID).Error; err != nil {
+				return err
+			} else {
+				var buyFrom Account
+				if err := db.Where(&Account{
+					UserID:      taker.UserID,
+					AssetTicker: order.Buyer.AssetTicker}).
+					Take(&buyFrom).Error; err != nil {
+					if errors.Is(err, gorm.ErrRecordNotFound) {
+						return insufficientFund
+					} else {
+						return err
+					}
+				} else if buyFrom.Amount < order.BuyAmount {
+					return insufficientFund
+				} else {
+					sellTo := Account{
+						UserID:      taker.UserID,
+						AssetTicker: order.Seller.AssetTicker,
+					}
+					if err := db.Where(sellTo).Take(&sellTo).
+						Error; errors.Is(err, gorm.ErrRecordNotFound) {
+						if err := tx.Create(&sellTo).Error; err != nil {
+							return err
+						}
+					} else if err != nil {
+						return err
+					}
+					sellTo.Amount += order.SellAmount
+					order.Buyer.Amount += order.BuyAmount
+					if err := tx.Save(&sellTo).Error; err != nil {
+						return err
+					} else if err :=
+						tx.Save(&order.Buyer).Error; err != nil {
+						return err
+					} else if err :=
+						tx.Delete(&order).Error; err != nil {
+						return err
+					} else {
+						return nil
+					}
+				}
+			}
+		}); err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.JSON(http.StatusNotFound, gin.H{"error": err})
+			} else if errors.Is(err, insufficientFund) {
+				c.JSON(http.StatusPaymentRequired, gin.H{"error": err})
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			}
+		} else {
+			c.Status(http.StatusNoContent)
 		}
 	})
 	r.Run(":" + port)
